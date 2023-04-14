e397a60eb4dc8e8e3b4df13167eec38a26c01e0c
Roberto Cestari <robertotcestari@gmail.com>
2023-04-14 17:06:39 -0300

    wip

--- app/routes/_auth+/login.tsx
+++ app/routes/_auth+/login.tsx
@@ -1,4 +1,5 @@
 import { Form, Link, useActionData } from "@remix-run/react";
+import { useState } from "react";
 import { login } from "~/services/auth.server";
 
 export async function action({ request }: { request: Request }) {
@@ -13,30 +14,74 @@ export async function action({ request }: { request: Request }) {
 }
 
 export default function Login() {
+  const [opened, setOpened] = useState(false);
   let errors = useActionData();
 
   return (
-    <div className="bg-gray-800 min-h-screen text-white flex items-center justify-center">
+    <div className="dark:bg-[#0e141a]  min-h-screen text-white flex items-center justify-center">
       <div className="flex flex-col">
-        <h1 className="text-3xl">Login</h1>
-        {errors && <div className="text-red-500">{errors}</div>}
-        <form method="POST" className="text-gray-900 flex flex-col">
-          <input type="email" name="email" className="mb-3" />
-          <input type="password" name="password" />
-          <button type="submit" className="bg-blue-200 mt-10">
-            login
-          </button>
-        </form>
-        <Link to="/forgot-password">
-          Esqueci minha senha
-        </Link>
-        <div>
+        <img src="/codante.svg" alt="" className="w-72 mx-auto mb-16" />
+        {/* <p className="text-sm font-light text-slate-400 mb-2">
+          Login com Github
+        </p> */}
+        <div
+          className={`${
+            opened ? "hidden" : ""
+          } bg-[#17212B] border-[1.5px] dark:border-slate-700 p-10 rounded-2xl max-w-md md:w-[450px]`}
+        >
           <Form action="/auth/github" method="post">
-            <button className="mt-4 rounded bg-blue-900 text-white p-4">
+            <button className="rounded bg-gray-700 text-white p-4 w-full flex items-center justify-center gap-4">
+              <img src="/img/github-logo.svg" alt="" />
               Login with GitHub
             </button>
           </Form>
         </div>
+
+        <div
+          className={`${
+            opened ? "" : "hidden"
+          } bg-[#17212B] border-[1.5px] dark:border-slate-700 p-10 rounded-2xl max-w-md md:w-[450px]`}
+        >
+          {errors && <div className="text-red-500">{errors}</div>}
+          <form method="POST" className="text-gray-900 flex flex-col ">
+            <label className="text-slate-300 text-sm mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="rounded p-2 mb-8 dark:bg-[#0e141a] border dark:border-slate-700 dark:text-white "
+              id="email"
+              type="email"
+              name="email"
+            />
+            <label className="text-slate-300 text-sm mb-2" htmlFor="password">
+              Senha
+            </label>
+            <input
+              id="password"
+              type="password"
+              name="password"
+              className="rounded p-2 mb-8 dark:bg-[#0e141a] border dark:border-slate-700 dark:text-white "
+            />
+            <button type="submit" className="bg-blue-200 mt-10">
+              login
+            </button>
+          </form>
+          <Link to="/forgot-password">Esqueci minha senha</Link>
+          <div></div>
+        </div>
+        <p
+          className={` text-xs font-light text-slate-400 mb-2 text-right mt-4`}
+        >
+          ... ou, se preferir, faça{" "}
+          <button
+            className="underline"
+            onClick={() => {
+              setOpened(!opened);
+            }}
+          >
+            login com {opened ? "github" : "email e senha"}
+          </button>
+        </p>
       </div>
     </div>
   );
