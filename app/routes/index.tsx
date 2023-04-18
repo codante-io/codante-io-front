24d858841b604080f561290807aa429bab3b370e
Ícaro Harry <icaro.harry@toptal.com>
2023-04-18 12:45:17 -0300

    Increase tracks margin-bottom

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -160,7 +160,7 @@ export default function Index() {
             <span className="font-bold italic">mini projetos</span> para
             aprender temas específicos em programação.
           </p>
-          <section className="grid grid-cols-1 gap-4">
+          <section className="grid grid-cols-1 gap-4 mb-16">
             {homeInfo?.featured_tracks?.map((track) => (
               <TrackCard key={track.id} track={track} />
             ))}
