3798f9025725f0e4e838deaad355c7621e8887cc
Roberto Cestari <robertotcestari@gmail.com>
2023-04-26 11:30:31 -0300

    extract not found component

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -1,23 +1,10 @@
 import { json } from "@remix-run/node";
-import { Link } from "@remix-run/react";
+import NotFound from "~/components/not-found";
 
 export const loader = () => {
   return json(null, { status: 404 });
 };
 
-export default function NotFound() {
-  return (
-    <div className="container py-20 mx-auto">
-      <p className="mb-6 text-lg font-light text-brand">404</p>
-      <h1 className="mb-6 text-5xl font-bold font-lexend dark:text-slate-300">
-        Página não encontrada
-      </h1>
-      <p className="mb-10 dark:text-slate-500">
-        Desculpe, não encontramos nada por aqui...
-      </p>
-      <Link className="text-sm font-medium text-brand" to="/">
-        <span aria-hidden="true">←</span> Voltar para a Home
-      </Link>
-    </div>
-  );
+export default function NotFoundPage() {
+  return <NotFound />;
 }
