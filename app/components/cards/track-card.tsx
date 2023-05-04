0a4d371e55202dc3186f26e6e1fa9bd1162a05dc
Roberto Cestari <robertotcestari@gmail.com>
2023-05-04 16:41:36 -0300

    improvements in track card

--- app/components/cards/track-card.tsx
+++ app/components/cards/track-card.tsx
@@ -20,11 +20,9 @@ function TrackCard({ track }: { track: Track }) {
   return (
     <div key={track?.id}>
       <Link
-        onClick={(e) => track?.status === "soon" && e.preventDefault()}
+        // onClick={(e) => track?.status === "soon" && e.preventDefault()}
         to={`/trilhas/${track?.slug}`}
-        className={
-          track?.status === "soon" ? "cursor-not-allowed" : "cursor-pointer"
-        }
+        className="cursor-pointer"
       >
         <article className="relative border-[1.5px] border-slate-300 dark:border-slate-600 rounded-2xl bg-slate-100 dark:bg-gray-800 mb-4 shadow-sm hover:border-blue-300 hover:shadow-lg dark:hover:border-blue-900 dark:hover:shadow-lg flex flex-col md:flex-row">
           {track?.status === "soon" && <CardItemRibbon text="Em breve" />}
