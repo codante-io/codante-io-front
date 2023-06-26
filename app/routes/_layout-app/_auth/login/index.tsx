7a17248f24ccd79f7f3aff522f8efc6810b49366
Roberto Cestari <robertotcestari@gmail.com>
2023-06-26 11:02:37 -0300

    canonical url to login

--- app/routes/_layout-app/_auth/login/index.tsx
+++ app/routes/_layout-app/_auth/login/index.tsx
@@ -16,6 +16,14 @@ import AuthCard from "../auth-card";
 import { authenticator } from "~/services/github-auth.server";
 import LoadingButton from "~/components/form/loading-button";
 
+// meta function
+export function links() {
+  return {
+    rel: "canonical",
+    href: "https://codante.io/login",
+  };
+}
+
 export async function action({ request }: { request: Request }) {
   let formData = await request.formData();
 
