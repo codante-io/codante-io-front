ed51cb6655c0ca7c51e47d29dc16528bd548a8b9
Roberto Cestari <robertotcestari@gmail.com>
2023-06-26 10:26:45 -0300

    better 404 on workshops

--- app/routes/_layout-raw/layout.tsx
+++ app/routes/_layout-raw/layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet, isRouteErrorResponse, useRouteError } from "@remix-run/react";
 import { Error500 } from "~/components/errors/500";
 import NotFound from "~/components/errors/not-found";
+import Navbar from "~/components/navbar";
 import { useUserFromOutletContext } from "~/hooks/useUserFromOutletContext";
 
 export default function ChallengeLayout() {
@@ -9,13 +10,17 @@ export default function ChallengeLayout() {
 }
 
 export function ErrorBoundary() {
+  const user = useUserFromOutletContext();
   const error = useRouteError();
 
   if (isRouteErrorResponse(error)) {
     return (
-      <div>
-        <NotFound />
-      </div>
+      <>
+        <Navbar user={{ user }} />
+        <div className="min-h-screen">
+          <NotFound />
+        </div>
+      </>
     );
   }
 
