308381ddafb2b5c531768c54e29d77ddbfa6ab34
Roberto Cestari <robertotcestari@gmail.com>
2023-06-30 08:33:01 -0300

    wip

--- app/routes/_layout-app/_blog/blog_.$slug/index.tsx
+++ app/routes/_layout-app/_blog/blog_.$slug/index.tsx
@@ -1,6 +1,7 @@
 import type { LoaderArgs } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import MarkdownRenderer from "~/components/markdown-renderer";
+import ReactionsButton from "~/components/reactions-button";
 import { getPost } from "~/models/blog-post.server";
 
 export async function loader({ request, params }: LoaderArgs) {
@@ -13,11 +14,41 @@ export default function BlogPost() {
 
   return (
     <main className="container mx-auto">
-      <h1 className="mb-2 text-5xl">{blogPost.title}</h1>
-      <p className="text-lg italic font-light">{blogPost.short_description}</p>
-      <div className="prose prose-lg dark:prose-invert">
-        <MarkdownRenderer markdown={blogPost.content} />
+      <div className="mx-auto max-w-prose">
+        <BlogBreadcrumbs postTitle={blogPost.title} />
+        <div className="mt-6 prose lg:prose-lg dark:prose-invert">
+          <h1>{blogPost.title}</h1>
+          <p className="lead">{blogPost.short_description}</p>
+          <ReactionsButton
+            reactableId={blogPost.id}
+            reactableType="BlogPost"
+            reactions={blogPost.reactions}
+          />
+          <MarkdownRenderer markdown={blogPost.content} />
+        </div>
       </div>
     </main>
+  );
+}
+
+function BlogBreadcrumbs({ postTitle }: { postTitle?: string }) {
+  return (
+    <div className="flex items-center space-x-2 text-gray-500">
+      <Link
+        to="/"
+        className="hover:underline hover:text-gray-800 dark:hover:text-gray-100"
+      >
+        Home
+      </Link>
+      <span className="text-brand">{`>`}</span>
+      <Link
+        to="/blog"
+        className="hover:underline hover:text-gray-800 dark:hover:text-gray-100"
+      >
+        Blog
+      </Link>
+      <span className="text-brand">{`>`}</span>
+      <span>{postTitle}</span>
+    </div>
   );
 }
