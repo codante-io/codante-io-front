31c3f3bef11d5674b15e54ab5d8575cdf66ea289
Roberto Cestari <robertotcestari@gmail.com>
2023-07-20 07:00:35 -0300

    privacy policy page (#61)

---
+++ app/routes/_layout-app/_juridico/politica-de-privacidade/index.tsx
@@ -0,0 +1,231 @@
+import React from "react";
+
+export default function PrivacyPolicyPage() {
+  return (
+    <div className="container mx-auto">
+      <main className="prose max-w-none dark:prose-invert">
+        <h1 id="política-de-privacidade">Política de Privacidade</h1>
+        <hr />
+        <p>
+          Esta Política de Privacidade (“Política”) tem como objetivo informar a
+          você, Usuário do Codante (“Plataforma”), desenvolvida e provida pelo
+          setor de tecnologia do Codante, sobre quais informações são coletadas,
+          além de como elas são tratadas, em que situações são compartilhadas e
+          quando são excluídas.
+        </p>
+        <p>
+          A aceitação desta Política de Privacidade se dará no ato do seu clique
+          no botão de cadastro na Plataforma, de modo que o Usuário concorda e
+          permite o acesso às suas informações a partir de seu cadastro na
+          Plataforma, manifestando consentimento livre, expresso e informado. Se
+          o Usuário não concordar com a política de tratamento de dados descrita
+          nesse documento, não deverá utilizar a Plataforma.
+        </p>
+        <p>
+          Caso reste alguma dúvida entre em contato conosco, pelo e-mail{" "}
+          <a href="mailto:contato@codante.io">contato@codante.io</a>.
+        </p>
+        <h3 id="informações-coletadas">Informações Coletadas</h3>
+        <p>
+          O Codante coleta todas as informações inseridas pelo Usuário na
+          Plataforma, tais como dados cadastrais, submissões de projetos,
+          avaliações de cursos, comentários, exercícios etc. São coletadas pelo
+          Codante todas as informações ativamente disponibilizadas pelo Usuário
+          na utilização da Plataforma.
+        </p>
+        <p>
+          O Codante também coleta algumas informações automaticamente quando o
+          Usuário acessa e utiliza a Plataforma, tais como características do
+          dispositivo de acesso, do navegador, Protocolo de Internet (IP, com
+          data e hora), origem do IP, informações sobre cliques, páginas
+          acessadas, as páginas seguintes acessadas após a saída da Plataforma,
+          ou qualquer termo de busca digitado na Plataforma, dentre outros. O
+          Codante também poderá utilizar algumas tecnologias padrões para
+          coletar informações do Usuário, tais como cookies, pixel tags, beacons
+          e local shared objects, de modo a melhorar sua experiência de
+          navegação.
+        </p>
+        <p>
+          O Usuário pode, a qualquer momento, bloquear algumas destas
+          tecnologias para coleta automática de dados através do seu navegador.
+          Nesse caso é possível que algumas das funções oferecidas pela
+          Plataforma deixem de funcionar corretamente.
+        </p>
+        <p>
+          Desta forma, o Usuário desde já se encontra ciente acerca das
+          informações coletadas por meio da Plataforma e expressa consentimento
+          livre, expresso e informado com relação à coleta de tais informações.
+        </p>
+        <h3 id="utilização-e-tratamento-das-informações">
+          Utilização e Tratamento Das Informações
+        </h3>
+        <p>
+          O Codante considera todas as informações coletadas por meio da
+          Plataforma como confidenciais. Portanto, somente as utilizará da forma
+          aqui descrita e pelo Usuário autorizada. Todas as informações
+          cadastradas e coletadas na Plataforma são utilizadas para a prestação
+          de serviços pelo Codante, para melhorar a experiência de navegação do
+          Usuário na Plataforma, bem como para fins de marketing.
+        </p>
+        <p>
+          O Codante poderá trabalhar com empresas terceirizadas para a
+          divulgação de anúncios aos Usuários durante seu acesso à Plataforma.
+          Tais empresas poderão coletar informações sobre as visitas de Usuários
+          à Plataforma, no intuito de fornecer anúncios personalizados sobre
+          bens e serviços do interesse do Usuário.
+        </p>
+        <p>
+          O Usuário dá o consentimento livre, expresso e informado para que a
+          Codante e seus parceiros utilizem as informações coletadas por meio da
+          Plataforma para fins publicitários e comerciais, bem como para
+          adequada prestação de serviços pelo Codante.
+        </p>
+        <p>
+          O Usuário que não desejar receber mais e-mails promocionais, deverá
+          enviar um e-mail para{" "}
+          <a href="mailto:contato@codante.io">contato@codante.io</a>,
+          solicitando o não recebimento dessas mensagens ou então ele próprio
+          realizar sua exclusão através dos links disponibilizados nos e-mails
+          promocionais.
+        </p>
+        <p>
+          Importante lembrar que a Plataforma pode conter links para outras
+          páginas, que possuem Política de Privacidade com previsões diversas do
+          disposto neste documento. Dessa forma, o Codante não se responsabiliza
+          pela coleta, utilização, compartilhamento e armazenamento de dados dos
+          Usuários pelos responsáveis por tais páginas.
+        </p>
+        <h3 id="compartilhamento-de-informações">
+          Compartilhamento de Informações
+        </h3>
+        <p>
+          O Codante poderá compartilhar os dados coletados por meio da
+          Plataforma, nas seguintes situações:
+        </p>
+        <ul>
+          <li>
+            <p>
+              Com empresas parceiras do Codante, para fins publicitários,
+              conforme descrito no item 2 acima;
+            </p>
+          </li>
+          <li>
+            <p>
+              Quando necessário às atividades comerciais do Codante, como por
+              exemplo, mas não se limitando a, operadoras de cartão de crédito,
+              para o recebimento de pagamentos;
+            </p>
+          </li>
+          <li>
+            <p>
+              Para proteção dos interesses do Codante em qualquer tipo de
+              conflito, incluindo ações judiciais;
+            </p>
+          </li>
+          <li>
+            <p>
+              No caso de transações e alterações societárias envolvendo o
+              Codante, hipótese em que a transferência das informações será
+              necessária para a continuidade dos serviços;
+            </p>
+          </li>
+          <li>
+            <p>
+              Mediante ordem judicial ou pelo requerimento de autoridades
+              administrativas que detenham competência legal para sua
+              requisição.
+            </p>
+          </li>
+        </ul>
+        <h3 id="armazenamento-das-informações">
+          Armazenamento das Informações
+        </h3>
+        <p>
+          As informações dos Usuários serão armazenadas pelo Codante, em
+          servidores próprios ou por ela contratados.
+        </p>
+        <p>
+          O Codante emprega todos os esforços razoáveis de mercado para garantir
+          a segurança de seus sistemas na guarda de referidos dados, tais como:
+        </p>
+        <ul>
+          <li>
+            <p>
+              Utilização de métodos padrões e de mercado para criptografar e
+              anonimizar os dados coletados;
+            </p>
+          </li>
+          <li>
+            <p>
+              Utilização de software de proteção contra acesso não autorizado
+              aos nossos sistemas;
+            </p>
+          </li>
+          <li>
+            <p>
+              Autorização de acesso somente a pessoas previamente estabelecidas
+              aos locais onde armazenamos as informações;
+            </p>
+          </li>
+          <li>
+            <p>
+              Aqueles que entrarem em contato com as informações deverão se
+              comprometer a manter sigilo absoluto. A quebra do sigilo
+              acarretará responsabilidade civil e o responsável será processado
+              nos moldes da legislação brasileira.
+            </p>
+          </li>
+        </ul>
+        <p>
+          Esta Política representa esforço do Codante no sentido de resguardar
+          as informações dos Usuários de sua Plataforma. No entanto, em razão da
+          própria natureza da Internet, não é possível garantir que terceiros
+          mal intencionados não obtenham sucesso em acessar indevidamente as
+          informações armazenadas.
+        </p>
+        <h3 id="exclusão-das-informações">Exclusão das Informações</h3>
+        <p>
+          Mediante solicitação do Usuário, pelo email{" "}
+          <a href="mailto:contato@codante.io">contato@codante.io</a>, as
+          informações referidas na presente Política serão excluídas pelo
+          Codante quando deixarem de ser úteis aos propósitos para os quais
+          foram coletadas.
+        </p>
+        <p>
+          O Codante fará os melhores esforços para atender a todos os pedidos de
+          exclusão, no menor espaço de tempo possível. Tal exclusão, no entanto,
+          removerá também o cadastro do Usuário da Plataforma, que não
+          conseguirá mais acessá-la, inclusive no que diz respeito aos cursos
+          por ele adquiridos.
+        </p>
+        <p>
+          Porém, o Usuário deve estar ciente de que, mesmo em caso de requisição
+          de exclusão, o Codante respeitará o prazo de armazenamento mínimo de
+          informações determinado pela legislação brasileira.
+        </p>
+        <h3 id="atualização-desta-política">Atualização desta Política</h3>
+        <p>
+          Reservamo-nos o direito de alterar essa Política de Privacidade sempre
+          que necessário, como objetivo de fornecer maior segurança e
+          praticidade ao Usuário, o que poderá se dar sem prévia notificação ao
+          Usuário, salvo em caso de expressa vedação legal. Por isso é
+          importante que o Usuário leia a Política a cada nova atualização,
+          conforme data de modificação informada ao final deste documento.
+        </p>
+        <h3 id="legislação-e-foro-competentes">
+          Legislação e Foro Competentes
+        </h3>
+        <p>
+          Essa Política de Privacidade será regida, interpretada e executada de
+          acordo com as leis da República Federativa do Brasil,
+          independentemente dos conflitos dessas leis com leis de outros estados
+          ou países, sendo competente o Foro do local de residência do Usuário,
+          no Brasil, para dirimir qualquer dúvida decorrente deste instrumento.
+          O Usuário consente, expressamente, com a competência desse juízo, e
+          renuncia, neste ato, à competência de qualquer outro foro, por mais
+          privilegiado que seja ou venha a ser.
+        </p>
+      </main>
+    </div>
+  );
+}
