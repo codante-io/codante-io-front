df77009e84164293ae92bf53dc8bc125aa970cb3
felipe <felipeavmuller@gmail.com>
2023-08-02 10:45:01 -0300

    add og: to MP

--- app/routes/_layout-app/_mini-projetos/mini-projetos/index.tsx
+++ app/routes/_layout-app/_mini-projetos/mini-projetos/index.tsx
@@ -5,12 +5,30 @@ import {
   getChallenges,
   type ChallengeCardInfo,
 } from "~/models/challenge.server";
+import { getOgGeneratorUrl } from "~/utils/path-utils";
 
 export function meta() {
+  const title = "Mini Projetos | Codante.io";
+  const description =
+    "Mini Projetos de programação para você aprender praticando. Escolha um Mini Projeto para você se desafiar e aprender ainda mais.";
+  const imageUrl = getOgGeneratorUrl("Mini Projetos");
+
   return {
-    title: "Mini Projetos | Codante.io",
-    description:
-      "Mini Projetos de programação para você aprender praticando. Escolha um Mini Projeto para você se desafiar e aprender ainda mais.",
+    title: title,
+    description: description,
+    "og:title": title,
+    "og:description": description,
+    "og:image": imageUrl,
+    "og:type": "website",
+    "og:url": `https://codante.io/mini-projetos`,
+
+    "twitter:card": "summary_large_image",
+    "twitter:domain": "codante.io",
+    "twitter:url": `https://codante.io/mini-projetos`,
+    "twitter:title": title,
+    "twitter:description": description,
+    "twitter:image": imageUrl,
+    "twitter:image:alt": "Mini Projetos Codante",
   };
 }
 
