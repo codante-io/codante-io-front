0270a8844605406885bfbcdbad4706e1cc6accdc
felipe <felipeavmuller@gmail.com>
2023-08-18 10:00:09 -0300

    add error boundary

--- app/routes/_layout-app/_blog/blog_.$slug/index.tsx
+++ app/routes/_layout-app/_blog/blog_.$slug/index.tsx
@@ -1,6 +1,13 @@
 import type { LoaderArgs, MetaFunction } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
+import {
+  Link,
+  isRouteErrorResponse,
+  useLoaderData,
+  useRouteError,
+} from "@remix-run/react";
 import { TiArrowBackOutline } from "react-icons/ti";
+import { Error500 } from "~/components/errors/500";
+import NotFound from "~/components/errors/not-found";
 import MarkdownRenderer from "~/components/markdown-renderer";
 import ReactionsButton from "~/components/reactions-button";
 import { getPost } from "~/models/blog-post.server";
@@ -34,6 +41,20 @@ export const meta: MetaFunction<typeof loader> = ({ data, params }) => {
     "twitter:image:alt": data.blogPost.title,
   };
 };
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div>
+        <NotFound />
+      </div>
+    );
+  }
+
+  return <Error500 error={error} />;
+}
 
 export async function loader({ request, params }: LoaderArgs) {
   const blogPost = await getPost(request, params.slug!);
