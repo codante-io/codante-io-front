3cc2dc4ad394abd82c3c8e116def3f3e1da35dc1
Felipe Muller <felipeavmuller@gmail.com>
2023-09-15 12:05:55 -0300

    add placeholder (#92)

--- app/components/vimeo-player/index.tsx
+++ app/components/vimeo-player/index.tsx
@@ -62,32 +62,32 @@ export default function VimeoPlayer({
         <div className="vimeo-full-width" ref={playerRef}></div>
       </div>
 
-      {thumbnailURL && (
-        <div className="flex items-center justify-center h-full overflow-hidden lg:rounded-xl dark:bg-background-800 ">
+      <div className="flex items-center justify-center h-full overflow-hidden lg:rounded-xl dark:bg-background-800 ">
+        {thumbnailURL && (
           <img
             key={thumbnailURL}
             className="opacity-40 dark:opacity-30"
             src={thumbnailURL}
             alt=""
           />
-          {vimeoUrl ? (
-            <PlayCircleIcon className="absolute w-20 h-20 text-brand" />
-          ) : (
-            <div className="absolute z-20 p-3 bg-white border border-gray-200 rounded-lg shadow-2xl shadow-background-700 dark:border- dark:bg-background-800 dark:border-background-600 md:p-10">
-              <h3 className="font-bold md:text-2xl text-brand font-lexend">
-                Ops...{" "}
-              </h3>
-              <p className="mt-2 mb-4 text-sm text-gray-500 md:text-base">
-                Você precisa estar logado para acessar essa aula!
-              </p>
-              <LinkToLoginWithRedirect className="mt-4 text-gray-800 ">
-                <Button type="button">Entre com Github</Button>
-              </LinkToLoginWithRedirect>
-              {/* <LockClosedIcon className="w-20 h-20 text-brand" /> */}
-            </div>
-          )}
-        </div>
-      )}
+        )}
+        {vimeoUrl ? (
+          <PlayCircleIcon className="absolute w-20 h-20 text-brand" />
+        ) : (
+          <div className="absolute z-20 p-3 bg-white border border-gray-200 rounded-lg shadow-2xl shadow-background-700 dark:border- dark:bg-background-800 dark:border-background-600 md:p-10">
+            <h3 className="font-bold md:text-2xl text-brand font-lexend">
+              Ops...{" "}
+            </h3>
+            <p className="mt-2 mb-4 text-sm text-gray-500 md:text-base">
+              Você precisa estar logado para acessar essa aula!
+            </p>
+            <LinkToLoginWithRedirect className="mt-4 text-gray-800 ">
+              <Button type="button">Entre com Github</Button>
+            </LinkToLoginWithRedirect>
+            {/* <LockClosedIcon className="w-20 h-20 text-brand" /> */}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
