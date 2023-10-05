2e8bd96ea6ff9e98bc1204db866966d15a30e406
Ícaro Harry <icaro.harry@toptal.com>
2023-10-04 22:22:21 -0300

    Landing page for Rinha: v1

---
+++ app/routes/rinha-frontend/useMousePosition.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const useMousePosition = () => {
+  const [mousePosition, setMousePosition] = React.useState({
+    x: null,
+    y: null,
+  });
+
+  React.useEffect(() => {
+    const updateMousePosition = (ev) => {
+      setMousePosition({ x: ev.clientX, y: ev.clientY });
+    };
+    window.addEventListener("mousemove", updateMousePosition);
+
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
+
+  return mousePosition;
+};
+
+export default useMousePosition;
