a1bdc0f68ccd072083643b73979d95277882adb1
Roberto Cestari <robertotcestari@gmail.com>
2023-10-09 12:07:42 -0300

    add pages feature

--- app/routes/_layout-app/_docs/docs_.$slug/index.tsx
+++ app/routes/_layout-app/_docs/docs_.$slug/index.tsx
@@ -7,19 +7,19 @@ import {
 import { Error500 } from "~/components/errors/500";
 import NotFound from "~/components/errors/not-found";
 import Post from "~/components/post";
-import { getPost } from "~/models/blog-post.server";
+import { getPage } from "~/models/blog-post.server";
 import { getOgGeneratorUrl } from "~/utils/path-utils";
 import { abort404 } from "~/utils/responses.server";
 
 export const meta: MetaFunction<typeof loader> = ({ data, params }) => {
   // para não quebrar se não houver blogPost ainda.
-  if (!data?.blogPost) {
+  if (!data?.page) {
     return {};
   }
 
-  const title = `${data.blogPost.title} | Codante.io`;
-  const description = data.blogPost.short_description;
-  const imageUrl = getOgGeneratorUrl(data.blogPost.title, "Blog");
+  const title = `${data.page.title} | Codante.io`;
+  const description = data.page.short_description;
+  const imageUrl = getOgGeneratorUrl(data.page.title, "Página");
 
   return {
     title: title,
@@ -28,15 +28,15 @@ export const meta: MetaFunction<typeof loader> = ({ data, params }) => {
     "og:description": description,
     "og:image": imageUrl,
     "og:type": "website",
-    "og:url": `https://codante.io/blog/${params.slug}`,
+    "og:url": `https://codante.io/docs/${params.slug}`,
 
     "twitter:card": "summary_large_image",
     "twitter:domain": "codante.io",
-    "twitter:url": `https://codante.io/blog/${params.slug}`,
+    "twitter:url": `https://codante.io/docs/${params.slug}`,
     "twitter:title": title,
     "twitter:description": description,
     "twitter:image": imageUrl,
-    "twitter:image:alt": data.blogPost.title,
+    "twitter:image:alt": data.page.title,
   };
 };
 
@@ -55,20 +55,25 @@ export function ErrorBoundary() {
 }
 
 export async function loader({ request, params }: LoaderArgs) {
-  const blogPost = await getPost(request, params.slug!);
-  if (!blogPost) {
+  const page = await getPage(request, params.slug!);
+  if (!page) {
     return abort404();
   }
 
-  return { blogPost };
+  return { page };
 }
 
-export default function BlogPost() {
-  const { blogPost } = useLoaderData<typeof loader>();
+export default function PagePost() {
+  const { page } = useLoaderData<typeof loader>();
 
   return (
     <main className="container mx-auto">
-      <Post blogPost={blogPost} withBreadcrumbs={true} withReactions={true} />
+      <Post
+        blogPost={page}
+        withBreadcrumbs={false}
+        withReactions={false}
+        withAuthor={false}
+      />
     </main>
   );
 }
