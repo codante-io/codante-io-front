c13c9d7ff5693036ac0519eabe6948382293c776
Roberto Cestari <robertotcestari@gmail.com>
2023-11-07 07:08:26 -0300

    wip

--- app/components/navbar/profile-menu.tsx
+++ app/components/navbar/profile-menu.tsx
@@ -57,19 +57,6 @@ export default function ProfileMenu({ user }: { user: User }) {
           </Menu.Item>
           <Menu.Item>
             {({ active }) => (
-              <Link
-                to="/minha-assinatura"
-                className={classNames(
-                  active ? "dark:bg-background-800/50 bg-background-50" : "",
-                  "block px-4 py-2 text-sm dark:text-gray-50 text-gray-700"
-                )}
-              >
-                Assinatura
-              </Link>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
               <Form action="/logout" method="post">
                 <button
                   className={classNames(
--- app/routes/_layout-app/_dashboard/minha-conta/index.tsx
+++ app/routes/_layout-app/_dashboard/minha-conta/index.tsx
@@ -15,6 +15,10 @@ import { authenticator } from "~/services/github-auth.server";
 import AuthCard from "../../_auth/auth-card";
 import { changeName, changePassword } from "./services.server";
 import type { User } from "~/models/user.server";
+import { FiExternalLink } from "react-icons/fi";
+import type { Subscription } from "~/models/subscription.server";
+import { getSubscription } from "~/models/subscription.server";
+import { toTitleCase } from "~/utils/string-utils";
 
 export async function action({ request }: { request: Request }) {
   const formData = await request.formData();
@@ -59,8 +63,9 @@ export async function loader({ request }: { request: Request }) {
   });
 
   const userData: User | null = await user({ request });
+  const subscription = await getSubscription({ request });
 
-  return { user: userData };
+  return { user: userData, subscription };
 }
 
 export default function Conta() {
@@ -74,7 +79,7 @@ export default function Conta() {
       ? transition.state
       : "idle";
 
-  const { user } = useLoaderData<typeof loader>();
+  const { user, subscription } = useLoaderData<typeof loader>();
   const actionData = useActionData();
   const { showSuccessToast } = useToasterWithSound();
   const changePasswordErrors = actionData?.changePasswordErrors;
@@ -96,18 +101,38 @@ export default function Conta() {
   }, [isChangeNameSuccess, isChangePasswordSuccess, showSuccessToast]);
 
   return (
-    <div className="container mx-auto mb-16">
-      <MyAccountSection
-        user={user}
-        changeNameErrors={changeNameErrors}
-        changeNameStatus={changeNameStatus}
-      />
+    <>
+      <div className="container mx-auto mb-16">
+        <header className="">
+          <h1 className="text-3xl font-lexend">
+            {/* <span className="hidden ml-3 font-light text-blue-500 md:inline">
+              {" "}
+              &#8226;{" "}
+            </span> */}
+            Minha Conta
+          </h1>
 
-      <PasswordChangeSection
-        changePasswordStatus={changePasswordStatus}
-        changePasswordErrors={changePasswordErrors}
-      />
-    </div>
+          <span className="flex items-center">
+            <span className="hidden text-sm font-light md:inline dark:text-gray-300">
+              {user?.name}
+            </span>
+            {user?.is_pro === 1 && <ProBadge />}
+          </span>
+        </header>
+        {subscription && <SubscriptionSection subscription={subscription} />}
+
+        <MyAccountSection
+          user={user}
+          changeNameErrors={changeNameErrors}
+          changeNameStatus={changeNameStatus}
+        />
+
+        <PasswordChangeSection
+          changePasswordStatus={changePasswordStatus}
+          changePasswordErrors={changePasswordErrors}
+        />
+      </div>
+    </>
   );
 }
 
@@ -126,16 +151,8 @@ function MyAccountSection({
         <MdKeyboardDoubleArrowRight
           size={24}
           className="inline-block mr-2 text-blue-300 dark:text-blue-800"
-        />{" "}
-        Minha Conta
-        <span className="hidden ml-3 font-light text-blue-500 md:inline">
-          {" "}
-          &#8226;{" "}
-        </span>
-        <span className="hidden ml-3 text-base font-light md:inline dark:text-gray-300">
-          {user?.name}
-        </span>
-        {user?.is_pro === 1 && <ProBadge />}
+        />
+        Dados
       </h2>
 
       <AuthCard className="max-w-xl mt-6">
@@ -229,6 +246,77 @@ function PasswordChangeSection({
             </LoadingButton>
           </div>
         </Form>
+      </AuthCard>
+    </>
+  );
+}
+
+function SubscriptionSection({
+  subscription,
+}: {
+  subscription: Subscription | null;
+}) {
+  return (
+    <>
+      <h2 className="flex items-center mt-12 text-xl">
+        <MdKeyboardDoubleArrowRight
+          size={24}
+          className="inline-block mr-2 text-blue-300 dark:text-blue-800"
+        />{" "}
+        Assinatura
+      </h2>
+
+      <AuthCard className="max-w-xl mt-6">
+        <p className="mb-6 text-sm font-light text-gray-600 dark:text-gray-400 text-inter">
+          Codante
+          <span className="px-1 mx-1 text-xs rounded py-0.5 bg-amber-400 text-background-50 dark:text-background-900">
+            PRO
+          </span>{" "}
+          - Vitalício
+        </p>
+        <p className="mb-2 text-sm font-light text-gray-600 dark:text-gray-400 text-inter">
+          Status:{" "}
+          <span
+            className={`${
+              subscription.status === "active"
+                ? "text-green-400"
+                : "dark:text-amber-500 text-amber-600"
+            }`}
+          >
+            {subscription.translated_status}
+          </span>
+        </p>
+        <p className="mb-2 text-sm font-light text-gray-500 dark:text-gray-400 text-inter">
+          Início:{" "}
+          <span className="text-gray-700 dark:text-white">
+            {subscription.starts_at}
+          </span>{" "}
+        </p>
+        <p className="mb-2 text-sm font-light text-gray-500 dark:text-gray-400 text-inter">
+          Término:{" "}
+          <span className="text-gray-700 dark:text-white">Vitalício</span>
+        </p>
+        <p className="mb-2 text-sm font-light text-gray-500 dark:text-gray-400 text-inter">
+          Forma de Pagamento:{" "}
+          <span className="text-gray-700 dark:text-white">
+            {toTitleCase(subscription.payment_method ?? "")}
+          </span>
+        </p>
+        {subscription.status !== "active" &&
+          subscription.payment_method?.toLowerCase() === "boleto" &&
+          subscription.boleto_url && (
+            <p className="mb-2 text-sm font-light text-inter">
+              <a
+                href={subscription.boleto_url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center gap-2 text-gray-500 hover:text-gray-700 dark:hover:text-white hover:underline dark:text-gray-400"
+              >
+                Link do Boleto
+                <FiExternalLink />
+              </a>
+            </p>
+          )}
       </AuthCard>
     </>
   );
