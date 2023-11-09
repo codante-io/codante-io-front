a82ec29f393f8c89cd205fd815c4610de867bd84
felipe <felipeavmuller@gmail.com>
2023-11-09 15:26:43 -0300

    adjustments faq

--- app/routes/_layout-app/_subscription/faq-questions.ts
+++ app/routes/_layout-app/_subscription/faq-questions.ts
@@ -2,22 +2,22 @@ const faqQuestions = [
   {
     question: "O que é o plano 'PRO (Vitalício)'?",
     answer:
-      "O plano PRO (Vitalício) é uma assinatura única que oferece acesso vitalício a todos os recursos e benefícios exclusivos do serviço.",
+      "O plano PRO (Vitalício) é uma assinatura única que oferece acesso vitalício a todos os recursos e benefícios exclusivos do Codante.",
   },
   {
     question: "Quais são as vantagens de se tornar um assinante PRO?",
     answer:
-      "Como assinante PRO, você terá acesso a todos os mini projetos, poderá submeter suas resoluções, receberá a resolução oficial com vídeo, ganhará certificados, terá acesso a todos os workshops, além de outros benefícios como o Pro Badge, canais PRO na comunidade e acesso ao Ranking Premiado.",
+      "Como assinante PRO você poderá participar de todos os Mini Projetos, terá acesso às resoluções oficiais com vídeo, realizadas pela equipe Codante, e acesso total a todos os Workshops da plataforma. Os assinantes PRO também ficarão elegíveis a receberem certificados, além de outros benefícios como o Pro Badge, canais PRO na comunidade e acesso ao Ranking Premiado.",
   },
   {
-    question: "O que inclui o acesso a mini projetos?",
+    question: "O que inclui o acesso a Mini Projetos?",
     answer:
-      "O acesso a mini projetos permite que você veja e trabalhe em todos os mini projetos oferecidos pela plataforma, permitindo aprimorar suas habilidades práticas em programação.",
+      "O acesso a Mini Projetos permite que você participe de todos os Mini Projetos oferecidos pela plataforma, submeta sua própria resolução e tenha acesso à resolução oficial desenvolvida pela equipe Codante.",
   },
   {
     question: "Os assinantes gratuitos podem submeter resoluções de projetos?",
     answer:
-      "Sim, mesmo no plano gratuito, os usuários podem submeter suas soluções para os projetos.",
+      "Sim, mesmo no plano gratuito, os usuários podem submeter suas soluções dos projetos.",
   },
   {
     question: "O que significa 'Resolução Oficial com vídeo'?",
@@ -30,9 +30,9 @@ const faqQuestions = [
       "Não, os certificados estão disponíveis apenas para assinantes do plano PRO.",
   },
   {
-    question: "Como funciona o acesso ilimitado aos workshops?",
+    question: "Como funciona o acesso ilimitado aos Workshops?",
     answer:
-      "No plano PRO, você pode acessar qualquer workshop disponibilizado na plataforma sem restrições, enquanto no plano gratuito, o acesso é limitado.",
+      "No plano PRO, você pode acessar qualquer Workshop disponibilizado na plataforma sem restrições, enquanto no plano gratuito, o acesso é limitado.",
   },
   {
     question: "O que é o Pro Badge?",
@@ -43,22 +43,17 @@ const faqQuestions = [
     question:
       "Qual é a diferença entre o plano gratuito e o plano PRO em termos de suporte da comunidade?",
     answer:
-      "O plano PRO oferece acesso a canais exclusivos da comunidade, onde provavelmente se pode esperar um suporte mais direcionado ou interação com uma comunidade de desenvolvedores mais engajada.",
+      "O plano PRO oferece acesso a canais exclusivos da comunidade no Discord, onde você poderá interagir diretamente com a equipe Codante e contar com o apoio de uma comunidade de desenvolvedores mais engajada.",
   },
   {
     question: "O que é o 'Ranking Premiado'?",
     answer:
-      "O Ranking Premiado é uma funcionalidade que reconhece e premia os usuários mais ativos ou com melhores desempenhos na plataforma, exclusivo para assinantes PRO.",
-  },
-  {
-    question: "Qual é a política de reembolso para a assinatura PRO?",
-    answer:
-      "Esta informação não é fornecida na imagem, então seria necessário consultar os termos de serviço ou suporte ao cliente da plataforma para obter detalhes sobre a política de reembolso.",
+      "O Ranking Premiado é uma funcionalidade que, periodicamente, reconhece e premia os usuários mais ativos ou com melhor desempenho nas atividades da plataforma, exclusivo para assinantes PRO.",
   },
   {
     question: "Como posso assinar o plano PRO?",
     answer:
-      "Você pode assinar o plano PRO clicando no botão 'Assinar PRO' e seguindo o processo de pagamento indicado na plataforma.",
+      "Você pode assinar o plano PRO clicando no botão 'Assinar PRO' e seguindo o processo de pagamento que será indicado na tela. Toda compra realizada nesse canal é processada pela plataforma Pagar.me, que oferece um ambiente seguro e confiável para a realização de pagamentos online.",
   },
 ];
 
