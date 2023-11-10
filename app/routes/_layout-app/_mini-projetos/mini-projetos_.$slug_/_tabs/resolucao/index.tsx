bfab7bea0417986ae5bae57898b0b07baead5a03
felipe <felipeavmuller@gmail.com>
2023-11-10 15:31:01 -0300

    add padding

--- app/routes/_layout-app/_mini-projetos/mini-projetos_.$slug_/_tabs/resolucao/index.tsx
+++ app/routes/_layout-app/_mini-projetos/mini-projetos_.$slug_/_tabs/resolucao/index.tsx
@@ -24,34 +24,29 @@ export default function Resolution() {
           Resolução do Mini Projeto
         </h1>
         <div className="flex flex-wrap pb-16 lg:mt-0 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-6 lg:px-0">
-          <section className="relative col-span-2 w-full">
+          <section className="relative col-span-2 w-full mb-4">
             <VimeoPlayer vimeoUrl={lesson?.video_url ?? ""} />
-            <div className="min-w-0 mt-6 lg:col-span-2 lg:px-2 lg:text-lg lg:mt-8">
-              <div className="px-2 mb-8 sm:px-0">
-                <h1 className="text-2xl font-semibold tracking-tight sm:text-3xl md:text-4xl font-lexend">
-                  {
-                    workshop?.lessons.find((lesson) => lesson.slug === slug)
-                      ?.name
-                  }
-                </h1>
-                <p className="mt-2 sm:text-lg md:text-xl lg:mt-4 lg:text-[22px] lg:leading-snug font-light dark:text-gray-300 text-gray-500">
-                  {
-                    workshop?.lessons.find((lesson) => lesson.slug === slug)
-                      ?.description
-                  }
-                </p>
-                <MarkdownRenderer
-                  markdown={
-                    workshop?.lessons.find((lesson) => lesson.slug === slug)
-                      ?.content || ""
-                  }
-                />
-              </div>
+            <div className="mt-8 md:px-28 px-2 sm:px-12 lg:px-2">
+              <h1 className="text-2xl font-semibold tracking-tight sm:text-3xl md:text-4xl font-lexend ">
+                {workshop?.lessons.find((lesson) => lesson.slug === slug)?.name}
+              </h1>
+              <p className="mt-2 sm:text-lg md:text-xl lg:mt-4 lg:text-[22px] lg:leading-snug font-light dark:text-gray-300 text-gray-500">
+                {
+                  workshop?.lessons.find((lesson) => lesson.slug === slug)
+                    ?.description
+                }
+              </p>
+              <MarkdownRenderer
+                markdown={
+                  workshop?.lessons.find((lesson) => lesson.slug === slug)
+                    ?.content || ""
+                }
+              />
             </div>
           </section>
 
           <section className="mx-auto">
-            <div className="flex-shrink-0 w-full mx-auto px-2 mb-8">
+            <div className="flex-shrink-0 mx-auto px-2 mb-8">
               {workshop?.lessons?.length && workshop?.lessons?.length > 0 && (
                 <>
                   <WorkshopLessonsHeader workshop={workshop} title="Vídeos" />
