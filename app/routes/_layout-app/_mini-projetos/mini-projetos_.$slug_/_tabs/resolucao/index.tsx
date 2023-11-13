1073acd8938b7b2bf8506224e2d06e7176e4b373
Felipe Muller <felipeavmuller@gmail.com>
2023-11-13 15:00:14 -0300

    FIX - Alinhamento video resoluçao MP (#130)

--- app/routes/_layout-app/_mini-projetos/mini-projetos_.$slug_/_tabs/resolucao/index.tsx
+++ app/routes/_layout-app/_mini-projetos/mini-projetos_.$slug_/_tabs/resolucao/index.tsx
@@ -1,5 +1,4 @@
-import { useOutletContext } from "@remix-run/react";
-import { useNavigate } from "@remix-run/react";
+import { useOutletContext, useNavigate } from "@remix-run/react";
 import MarkdownRenderer from "~/components/markdown-renderer";
 import VimeoPlayer from "~/components/vimeo-player";
 import WorkshopLessonsHeader from "~/components/workshop-lessons-header";
@@ -24,37 +23,30 @@ export default function Resolution() {
         <h1 className="flex items-center mb-4 text-2xl font-semibold font-lexend text-brand">
           Resolução do Mini Projeto
         </h1>
-      </div>
-      <div className="container">
-        <div className="flex flex-wrap pb-16 mx-auto sm:mt-12 sm:max-w-lg md:max-w-prose lg:mt-0 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-6 lg:px-0">
-          <section className="relative col-span-2">
+        <div className="flex flex-wrap pb-16 lg:mt-0 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-6 lg:px-0">
+          <section className="relative col-span-2 w-full mb-4">
             <VimeoPlayer vimeoUrl={lesson?.video_url ?? ""} />
-            <div className="min-w-0 mt-6 lg:col-span-2 lg:px-2 lg:text-lg lg:mt-8">
-              <div className="px-2 mb-8 sm:px-0">
-                <h1 className="text-2xl font-semibold tracking-tight sm:text-3xl md:text-4xl font-lexend">
-                  {
-                    workshop?.lessons.find((lesson) => lesson.slug === slug)
-                      ?.name
-                  }
-                </h1>
-                <p className="mt-2 sm:text-lg md:text-xl lg:mt-4 lg:text-[22px] lg:leading-snug font-light dark:text-gray-300 text-gray-500">
-                  {
-                    workshop?.lessons.find((lesson) => lesson.slug === slug)
-                      ?.description
-                  }
-                </p>
-                <MarkdownRenderer
-                  markdown={
-                    workshop?.lessons.find((lesson) => lesson.slug === slug)
-                      ?.content || ""
-                  }
-                />
-              </div>
+            <div className="mt-8 max-w-full md:max-w-xl lg:max-w-none px-2 mx-auto lg:mx-0">
+              <h1 className="text-2xl font-semibold tracking-tight sm:text-3xl md:text-4xl font-lexend ">
+                {workshop?.lessons.find((lesson) => lesson.slug === slug)?.name}
+              </h1>
+              <p className="mt-2 sm:text-lg md:text-xl lg:mt-4 lg:text-[22px] lg:leading-snug font-light dark:text-gray-300 text-gray-500">
+                {
+                  workshop?.lessons.find((lesson) => lesson.slug === slug)
+                    ?.description
+                }
+              </p>
+              <MarkdownRenderer
+                markdown={
+                  workshop?.lessons.find((lesson) => lesson.slug === slug)
+                    ?.content || ""
+                }
+              />
             </div>
           </section>
 
-          <section className="mx-auto ">
-            <div className="flex-shrink-0 w-full px-2 mb-8 ml-auto">
+          <section className="mx-auto max-w-xl">
+            <div className="flex-shrink-0 mx-auto px-2 mb-8">
               {workshop?.lessons?.length && workshop?.lessons?.length > 0 && (
                 <>
                   <WorkshopLessonsHeader workshop={workshop} title="Vídeos" />
