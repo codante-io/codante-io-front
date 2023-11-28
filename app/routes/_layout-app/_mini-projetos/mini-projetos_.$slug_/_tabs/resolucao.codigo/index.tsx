80deca7f7e72ebefb3db24112bcf8f8a2d136ea2
felipe <felipeavmuller@gmail.com>
2023-11-28 17:43:20 -0300

    fix mobile

--- app/routes/_layout-app/_mini-projetos/mini-projetos_.$slug_/_tabs/resolucao.codigo/index.tsx
+++ app/routes/_layout-app/_mini-projetos/mini-projetos_.$slug_/_tabs/resolucao.codigo/index.tsx
@@ -30,24 +30,28 @@ export default function ResolutionCode() {
           className="w-full h-full rounded-lg shadow blur-none relative z-10"
         ></iframe>
       </div>
-      <div className="mt-10 w-full flex gap-10">
+      <div className="mt-10 w-full flex md:gap-8 gap-4">
         <a
-          className={`dark:bg-[#17212B] flex gap-2 items-center shadow bg-white border-[1.5px] group dark:hover:border-brand-500 hover:border-brand-500 border-background-200 justify-between dark:border-background-700 w-full rounded-lg p-5`}
+          className={`dark:bg-[#17212B] flex flex-col-reverse md:flex-row text-center md:text-start gap-2 items-center shadow bg-white border-[1.5px] group dark:hover:border-brand-500 hover:border-brand-500 border-background-200 justify-between dark:border-background-700 w-full rounded-lg p-5`}
           href="https://codante.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <p className="text-lg">Acessar o código no GitHub</p>
+          <p className="text-xs md:text-base lg:text-lg">
+            Acessar o código no GitHub
+          </p>
           <BsGithub className="text-3xl group-hover:text-brand-500 text-background-200 dark:text-background-600" />
         </a>
 
         <a
-          className={`dark:bg-[#17212B] justify-between flex gap-2 items-center shadow bg-white border-[1.5px] group dark:hover:border-brand-500 hover:border-brand-500 border-background-200 dark:border-background-700 w-full rounded-lg p-5`}
+          className={`dark:bg-[#17212B] flex flex-col-reverse md:flex-row text-center md:text-start gap-2 items-center shadow bg-white border-[1.5px] group dark:hover:border-brand-500 hover:border-brand-500 border-background-200 justify-between dark:border-background-700 w-full rounded-lg p-5`}
           href="https://codante.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <p className="text-lg">Acessar o deploy da aplicação</p>
+          <p className="text-xs md:text-base lg:text-lg">
+            Acessar o deploy da aplicação
+          </p>
           <BsGlobe className="text-3xl group-hover:text-brand-500 text-background-200 dark:text-background-600" />
         </a>
       </div>
