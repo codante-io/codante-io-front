739f6ecf7563311c5e0fa1f6fdf76bbc2888be84
felipe <felipeavmuller@gmail.com>
2023-12-15 10:20:57 -0300

    fix width

--- app/routes/_layout-app/_dashboard/minha-conta/index.tsx
+++ app/routes/_layout-app/_dashboard/minha-conta/index.tsx
@@ -536,7 +536,7 @@ function LinkedinSection({
               href={user?.linkedin_url}
               target="_blank"
               rel="noopener noreferrer"
-              className="mt-4 flex items-center gap-2 text-gray-500 hover:text-gray-700 dark:hover:text-white hover:underline dark:text-gray-400"
+              className="mt-4 w-fit flex items-center gap-2 text-gray-500 hover:text-gray-700 dark:hover:text-white hover:underline dark:text-gray-400"
             >
               Visualizar perfil cadastrado
               <FiExternalLink />
