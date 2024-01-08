85c13ded678333ce906c0e767f01436cbf571900
Roberto Cestari <robertotcestari@gmail.com>
2024-01-08 11:51:57 -0300

    Change Submission Behavior (#159)
    * change submission behavior
    
    * adjust spacing
    
    * styling fixes
    
    * styling fixes

--- app/lib/utils/path-utils.ts
+++ app/lib/utils/path-utils.ts
@@ -1,15 +1,6 @@
 import type { UIMatch } from "@remix-run/react";
 import slugify from "slugify";
 
-// export function slugify(str: string) {
-//   return str
-//     .toLowerCase()
-//     .trim()
-//     .replace(/[^\w\s-]/g, "")
-//     .replace(/[\s_-]+/g, "-")
-//     .replace(/^-+|-+$/g, "");
-// }
-
 export function getOgGeneratorUrl(title: string, subtitle = "") {
   if (subtitle) {
     return `https://og.codante.io/api/${slugify(subtitle, {
