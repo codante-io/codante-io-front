020aa5362c6883c01fba48b9273b5f5263d9b3c9
felipe <felipeavmuller@gmail.com>
2024-01-09 14:58:47 -0300

    wip

--- app/components/_layouts/certificate/index.tsx
+++ app/components/_layouts/certificate/index.tsx
@@ -1,12 +1,4 @@
-import {
-  Page,
-  Text,
-  View,
-  Document,
-  StyleSheet,
-  Image,
-  Font,
-} from "@react-pdf/renderer";
+import { Page, Text, View, Document, Image, Font } from "@react-pdf/renderer";
 
 Font.register({
   family: "Alex Brush",
@@ -22,69 +14,19 @@ Font.register({
   fontWeight: "normal",
 });
 
-// Create styles
-const styles = StyleSheet.create({
-  page: {
-    display: "flex",
-    // alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "row",
-    backgroundColor: "#E4E4E4",
-  },
-  sectionBorder: {
-    width: "100%",
-    height: "50%",
-    border: "1px solid black",
-    backgroundColor: "#5282FF",
-    position: "relative",
-  },
-  bgImage: {
-    width: "100%",
-    height: "100%",
-    position: "absolute",
-    // zIndex: -1,
-  },
-  logoImage: {
-    width: "100px",
-    margin: "5px",
-  },
-  main: {
-    height: "100%",
-    margin: "16px",
-    backgroundColor: "white",
-  },
-  title: {
-    marginTop: "16px",
-    textAlign: "center",
-    fontSize: "38px",
-    // fontWeight: "semibold",
-    fontFamily: "Alex Brush",
-  },
-  text: {
-    textAlign: "left",
-    marginLeft: "16px",
-    marginRight: "16px",
-    fontSize: "14px",
-    marginTop: "16px",
-  },
-  username: {
-    textAlign: "center",
-    fontSize: "18px",
-    fontFamily: "Roboto Condensed",
-    textTransform: "uppercase",
-    marginTop: "16px",
-  },
-  firstBox: {
-    textAlign: "center",
-    borderBottom: "1px solid black",
-    width: "50%",
-    margin: "0 auto",
-    marginTop: "5px",
-  },
-  content: {
-    display: "flex",
-    flexDirection: "row",
-  },
+Font.register({
+  family: "Istok Web",
+  fonts: [
+    { src: "/fonts/IstokWeb-Regular.ttf" }, // font-style: normal, font-weight: normal
+    { src: "/fonts/IstokWeb-Bold.ttf", fontWeight: "bold" },
+  ],
+});
+
+Font.register({
+  family: "Rubik",
+  src: "/fonts/rubik-regular.ttf",
+  fontStyle: "normal",
+  fontWeight: "normal",
 });
 
 // Create Document Component
@@ -107,36 +49,167 @@ export default function CertificatePDF({
       year: "numeric",
     });
   }
-
   return (
     <Document>
-      <Page orientation="landscape" size="A4" style={styles.page}>
-        <View style={styles.sectionBorder}>
-          <Image src="/img/logobackground.png" style={styles.bgImage} />
-          <View style={styles.main}>
+      <Page
+        orientation="landscape"
+        size="A4"
+        style={{ fontFamily: "Istok Web" }}
+      >
+        <View
+          style={{
+            width: "100%",
+            height: "100%",
+            border: "1px solid black",
+            backgroundColor: "#1E2B38",
+            position: "relative",
+          }}
+        >
+          <Image
+            src="/img/logobackground.png"
+            style={{
+              width: "100%",
+              height: "100%",
+              position: "absolute",
+            }}
+          />
+          <View
+            style={{
+              height: "100%",
+              margin: "25px",
+              backgroundColor: "white",
+              borderRadius: "5px",
+            }}
+          >
             <Image
               src="/img/codante-certificado-logo.png"
-              style={styles.logoImage}
+              style={{
+                width: "150px",
+                height: "auto",
+                margin: "10px",
+              }}
             />
-            <Text style={styles.title}>Certificado de conclusão</Text>
-            <Text style={styles.text}>Certificamos que</Text>
-            <View style={styles.firstBox}>
-              <Text style={styles.username}>{username}</Text>
+            <Text
+              style={{
+                marginTop: "20px",
+                textAlign: "center",
+                fontSize: "48px",
+                fontFamily: "Alex Brush",
+              }}
+            >
+              Certificado de conclusão
+            </Text>
+            <Text
+              style={{
+                textAlign: "left",
+                marginHorizontal: "20px",
+                fontSize: "14px",
+                marginTop: "40px",
+              }}
+            >
+              Este certificado é concedido para
+            </Text>
+            <View
+              style={{
+                textAlign: "center",
+                borderBottom: "1px solid black",
+                width: "50%",
+                margin: "0 auto",
+                marginVertical: "20px",
+              }}
+            >
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontSize: "18px",
+                  fontFamily: "Roboto Condensed",
+                  textTransform: "uppercase",
+                  marginTop: "16px",
+                }}
+              >
+                {username}
+              </Text>
             </View>
             <View
               style={{
                 fontSize: "14px",
                 display: "flex",
+                flexWrap: "wrap",
                 flexDirection: "row",
-                marginHorizontal: "16px",
+                marginHorizontal: "20px",
                 marginTop: "20px",
+                lineHeight: "2",
               }}
             >
-              <Text>Em </Text>
-              <Text style={{ fontWeight: "bold" }}>{formatDate(date)} </Text>
-              <Text>completou o Projeto </Text>
-              <Text style={{ color: "#5282FF" }}>{title} </Text>
+              <Text>Em reconhecimento pela conclusão </Text>
+              <Text>do projeto </Text>
+              <Text
+                style={{
+                  fontWeight: "bold",
+                  color: "#5282FF",
+                }}
+              >
+                {title}{" "}
+              </Text>
+              <Text>que abordou, </Text>
+              <Text>de forma prática, </Text>
+              <Text>
+                {tags.length > 1 ? "as tecnologias " : "a tecnologia "}
+              </Text>
+              {tags.map((tag, index) => (
+                <Text key={index} style={{ fontWeight: "semibold" }}>
+                  {tag}
+                  {index === tags.length - 2
+                    ? " e "
+                    : index < tags.length - 1
+                    ? ", "
+                    : "."}
+                </Text>
+              ))}
             </View>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                marginHorizontal: "100px",
+                marginTop: "100px",
+              }}
+            >
+              <View
+                style={{
+                  borderTop: "1px solid black",
+                  width: "200px",
+                  textAlign: "center",
+                  fontFamily: "Rubik",
+                  fontSize: "12px",
+                  paddingTop: "10px",
+                }}
+              >
+                <Text>Icaro Harry</Text>
+              </View>
+              <View
+                style={{
+                  borderTop: "1px solid black",
+                  width: "200px",
+                  textAlign: "center",
+                  fontFamily: "Rubik",
+                  fontSize: "12px",
+                  paddingTop: "10px",
+                }}
+              >
+                <Text>Roberto Cestari</Text>
+              </View>
+            </View>
+            <Text
+              style={{
+                textAlign: "center",
+                marginTop: "50px",
+                fontSize: "12px",
+              }}
+            >
+              {formatDate(date)}
+            </Text>
           </View>
         </View>
       </Page>
