0f15e9634152a3fd8b408091e1986022daaf4875
Roberto Cestari <robertotcestari@gmail.com>
2024-07-15 10:13:56 -0300

    chore: Refactor userEnteredWorkshop to return void (#221)
    ```text
    Refactor the userEnteredWorkshop function to return void instead of Promise<Workshop | null>. This change is made to align with the updated axios post request in the function. The workshop variable is no longer needed to be returned.

--- app/routes/_layout-raw/_player/workshops_.$workshopSlug_.$slug/index.tsx
+++ app/routes/_layout-raw/_player/workshops_.$workshopSlug_.$slug/index.tsx
@@ -10,11 +10,11 @@ import type { Lesson } from "~/lib/models/lesson.server";
 import type { User } from "~/lib/models/user.server";
 import type { Workshop } from "~/lib/models/workshop.server";
 import { getWorkshop, userEnteredWorkshop } from "~/lib/models/workshop.server";
+import { getOgGeneratorUrl } from "~/lib/utils/path-utils";
 import { abort404 } from "~/lib/utils/responses.server";
 import MainContent from "../components/main-content";
 import Sidebar from "../components/sidebar";
 import styles from "../styles.css?url";
-import { getOgGeneratorUrl } from "~/lib/utils/path-utils";
 
 export function links() {
   return [{ rel: "stylesheet", href: styles }];
@@ -78,7 +78,7 @@ export async function loader({
     return abort404();
   }
 
-  await userEnteredWorkshop(workshop.slug, request);
+  userEnteredWorkshop(workshop.slug, request);
 
   return {
     workshop: workshop,
