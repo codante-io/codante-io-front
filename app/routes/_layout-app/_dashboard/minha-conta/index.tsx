b1cb4b03d752772f4e94dc171d292265f128a575
Roberto Cestari <robertotcestari@gmail.com>
2024-08-14 19:16:23 -0300

    fix bug

--- app/routes/_layout-app/_dashboard/minha-conta/index.tsx
+++ app/routes/_layout-app/_dashboard/minha-conta/index.tsx
@@ -264,7 +264,7 @@ export default function Conta() {
                     to={`https://github.com/${user.github_user}`}
                     target="_blank"
                   >
-                    @robertotcestari
+                    @{user.github_user}
                   </Link>
                 </p>
               )}
