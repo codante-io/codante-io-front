72bc5bd134ba6f2ff6da29de4be06027158d3729
Roberto Cestari <robertotcestari@gmail.com>
2024-08-20 08:19:42 -0300

    add react hooks to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,18 @@
 import jsESLint from "@eslint/js";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
 import eslintConfigPrettier from "eslint-config-prettier";
 import tsESLint from "typescript-eslint";
+import globals from "globals";
+import { fixupPluginRules } from "@eslint/compat";
 
 export default [
   jsESLint.configs.recommended,
   ...tsESLint.configs.recommended,
   eslintConfigPrettier,
+
   {
-    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
     ignores: [
       "**/node_modules/",
       "**/dist/",
@@ -20,7 +25,22 @@ export default [
       "**/.cache/",
       "**/dist",
     ],
+    plugins: {
+      react,
+      "react-hooks": fixupPluginRules(reactHooks),
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
     rules: {
+      ...reactHooks.configs.recommended.rules,
       "no-console": "warn",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/ban-ts-comment": "off",
