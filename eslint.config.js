77595b506a2c947516a119aae94026b22430afd5
Roberto Cestari <robertotcestari@gmail.com>
2024-08-20 07:26:52 -0300

    upgrade husky eslint (#240)
    * update eslint and husky v9
    
    * update eslint config

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  eslintConfigPrettier,
+  {
+    ignores: [
+      "**/node_modules/",
+      "**/dist/",
+      "**/build/",
+      "**/public/",
+      "**/.husky/",
+      "**/.husky/",
+      "**/.github/",
+      "**/.vscode/",
+      "**/.cache/",
+      "**/dist",
+    ],
+    rules: {
+      "no-console": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+    },
+  },
+];
