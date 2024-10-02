e4f189d324f6ee2c76038c72259cb018bb71fc59
Ícaro Harry <me@icaro.io>
2024-10-02 15:51:39 -0300

    Implement Google Recaptcha v3 (#248)

--- app/routes/_layout-app/_auth/layout.tsx
+++ app/routes/_layout-app/_auth/layout.tsx
@@ -1,10 +1,32 @@
-import { Outlet } from "@remix-run/react";
+import { json, Outlet, useLoaderData } from "@remix-run/react";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
+import { environment } from "~/lib/models/environment";
+
+export async function loader() {
+  const { RECAPTCHA_SITE_KEY } = environment();
+
+  return json({ ENV: { RECAPTCHA_SITE_KEY } });
+}
 
 export default function AuthLayout() {
+  const {
+    ENV: { RECAPTCHA_SITE_KEY },
+  } = useLoaderData<typeof loader>();
+
   return (
     <div className="bg-transparent text-white sm:pt-12 ">
       <div className="mx-auto max-w-md md:w-[450px] px-2 py-12 ">
-        <Outlet />
+        <GoogleReCaptchaProvider
+          reCaptchaKey={RECAPTCHA_SITE_KEY}
+          scriptProps={{
+            async: false,
+            defer: true,
+            appendTo: "head",
+            nonce: undefined,
+          }}
+        >
+          <Outlet />
+        </GoogleReCaptchaProvider>
       </div>
     </div>
   );
