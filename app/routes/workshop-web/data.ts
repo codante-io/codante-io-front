a61d6f752467727a93e769535e398a49544ba731
Carolzita Dev <42356399+devcarolzita@users.noreply.github.com>
2024-10-04 15:31:15 -0300

    Marketing page - Workshop Caról (#250)

---
+++ app/routes/workshop-web/data.ts
@@ -0,0 +1,80 @@
+export const faqs = [
+  {
+    question: "Preciso ter algum conhecimento prévio para participar do curso?",
+    answer:
+      "Não, o curso é voltado para iniciantes e você não precisa ter nenhum conhecimento prévio em programação. Vamos começar do zero e ensinar tudo o que você precisa saber para dar seus primeiros passos no desenvolvimento web!",
+  },
+  {
+    question: "Quais tecnologias e ferramentas vou aprender durante o curso?",
+    answer:
+      "Durante o curso, você aprenderá as bases do desenvolvimento web, incluindo HTML, CSS e JavaScript. Também vamos abordar boas práticas de desenvolvimento e como usar ferramentas essenciais para criar páginas web funcionais e atraentes.",
+  },
+  {
+    question: "O curso é totalmente prático ou tem teoria também ?",
+    answer:
+      "O curso é uma combinação de teoria e prática. Vamos explicar os conceitos de forma clara e, em seguida, aplicar o que foi aprendido em projetos práticos. Assim, você entende o 'porquê' por trás do código e ganha experiência prática",
+  },
+  {
+    question: "Após o curso, vou conseguir criar meu próprio site do zero?",
+    answer:
+      "Sim! Ao final do curso, você terá o conhecimento necessário para criar seu próprio site do zero, aplicando as tecnologias que aprendeu. Vamos construir projetos ao longo do curso para garantir que você esteja pronto para isso!",
+  },
+  {
+    question: "Haverá suporte caso eu tenha dúvidas durante o curso?",
+    answer:
+      "Com certeza! Estamos aqui para te ajudar. Você terá acesso a um canal de suporte onde poderá tirar suas dúvidas e receber orientação. Nosso objetivo é garantir que ninguém fique para trás!",
+  },
+];
+
+export const techs = [
+  { name: "html", position: [25, 12] },
+  { name: "css", position: [40, 10] },
+  { name: "javascript", position: [52, 12] },
+  { name: "tailwind", position: [16, 15] },
+  { name: "next", position: [70, 12] },
+  { name: "react", position: [85, 13] },
+];
+
+export const steps = [
+  {
+    title: "Cadastre-se e participe.",
+    description: "Você terá acesso a conteúdo e exercícios.",
+    img: "img/workshop-web/steps/start.svg",
+    alt: "Imagem de uma pessoa clicando no botão start",
+  },
+  {
+    title: "Aprenda a estudar.",
+    description:
+      "Desenvolva habilidades que melhoram sua compreensão com nossas técnicas.",
+    img: "img/workshop-web/steps/learn.svg",
+    alt: "Imagem de uma pessoa estudando",
+  },
+  {
+    title: "Exercícios práticos.",
+    description:
+      "Tenha acesso a diversos exercícios práticos para fixar o conteúdo.",
+    img: "img/workshop-web/steps/coding.png",
+    alt: "Ilustração de uma pessoa programando",
+  },
+  {
+    title: "Compartilhe sua dúvida.",
+    description:
+      "Estaremos disponíveis para ajudar você a resolver seus problemas.",
+    img: "img/workshop-web/steps/share.png",
+    alt: "Ilustração com várias logos de redes sociais (Twitter, Facebook, Instagram, LinkedIn)",
+  },
+  {
+    title: "Planejamento.",
+    description:
+      "Um conteúdo planejado para te tirar do zero, mesmo que você não saiba nada.",
+    img: "img/workshop-web/steps/calendar.png",
+    alt: "Ilustração de uma pessoa ao lado de um calendário",
+  },
+  {
+    title: "Certificado!",
+    description:
+      "Todos os participantes receberão um certificado de participação.",
+    img: "img/workshop-web/steps/winner.png",
+    alt: "Ilustração de duas pessoas comemorando ao lado de um troféu",
+  },
+];
