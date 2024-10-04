18dba8893897b7bd842d7715e236cfa5bea0fa15
Ícaro Harry <me@icaro.io>
2024-10-04 17:10:32 -0300

    Connect lead page to the backend (#251)

--- app/lib/models/lead.server.ts
+++ app/lib/models/lead.server.ts
@@ -1,11 +1,16 @@
 import { createAxios } from "~/lib/services/axios.server";
 
-export type Lead = {};
+export type Lead = {
+  id: number;
+  email: string;
+  name: string | null;
+  phone: string | null;
+};
 
 export async function registerChallengeLead(
   request: Request,
   email: string,
-): Promise<{}> {
+): Promise<{ success?: string; error?: string }> {
   try {
     const axios = await createAxios(request);
     await axios.post(`/leads`, {
@@ -23,6 +28,41 @@ export async function registerChallengeLead(
     if (e.response.status === 409) {
       errorMsg =
         "Esse email já está cadastrado em nossa lista. Basta fazer login para participar do projeto.";
+    }
+    return {
+      error: errorMsg,
+    };
+  }
+}
+
+export async function registerMarketingLead(
+  request: Request,
+  {
+    email,
+    tag,
+    name,
+    phone,
+  }: { email: string; tag: string; name?: string; phone?: string },
+): Promise<{ success?: string; error?: string }> {
+  try {
+    const axios = await createAxios(request);
+    await axios.post(`/leads`, {
+      email,
+      tags: [tag],
+      name,
+      phone,
+    });
+
+    return {
+      success: "Cadastro realizado. Vamos te enviar um email em breve",
+    };
+  } catch (e: any) {
+    let errorMsg =
+      "Não foi possível realizar o cadastro. Por favor, tente novamente.";
+
+    if (e.response.status === 409) {
+      errorMsg =
+        "Esse email já está cadastrado em nossa lista. Não se preocupe, você receberá nossos emails em breve.";
     }
     return {
       error: errorMsg,
